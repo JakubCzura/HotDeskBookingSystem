d120c2a4713a13d9dd9bf45cb088a88cae806c38
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2022-12-23 13:44:41 +0100

    Copy directory with tests to solution

---
+++ HotDeskBookingSystemTests/DateBetweenPersonReservationsVerificationTests.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotDeskBookingSystemTests
+{
+    public class DateBetweenPersonReservationsVerificationTests
+    {
+        [Fact]
+        public void HaveDeskReservedInThisTimeSpan_DeskDateNotBetweenTwoDates_ReturnFalse()
+        {
+            //arrange
+
+            Desk UserDesk = new() { ReservationStartDate = new DateTime(2022, 12, 5), ReservationEndDate = new DateTime(2022, 12, 7) };
+
+            List<Desk> UserDesks = new()
+            { new Desk() { ReservationStartDate = new DateTime(2022, 12, 10), ReservationEndDate =new DateTime(2022, 12, 12) },
+             new Desk() { ReservationStartDate = new DateTime(2022, 12, 14), ReservationEndDate =new DateTime(2022, 12, 16) },
+             new Desk() { ReservationStartDate = new DateTime(2022, 12, 18), ReservationEndDate =new DateTime(2022, 12, 19) } };
+
+            //act
+
+            bool result = DateBetweenPersonReservationsVerification.HaveDeskReservedInThisTimeSpan(UserDesk, UserDesks);
+            bool expectedResult = false;
+
+            //assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
---
+++ HotDeskBookingSystemTests/ReservationCancellatinonTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotDeskBookingSystemTests
+{
+    public class ReservationCancellationTests
+    {
+        [Fact]
+        public void CancelReservation_SelectedPersonDeskIsNull_ReturnsFalse()
+        {
+            //arrange
+            Desk Desk = null;
+
+            //act
+            bool result = ReservationCancellation.CancelReservation(Desk);
+            bool expectedResult = false;
+
+            //assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
